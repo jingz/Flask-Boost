31e5419b521ecb24eba4d639532fd6a6229d78b0
jingz <wsaryoo@gmail.com>
2018-03-18 17:01:55 +0700

    remove files

--- flask_turbo_boost/api_project/application/models/role.py
+++ flask_turbo_boost/api_project/application/models/role.py
@@ -1,9 +1,8 @@
 # coding: utf-8
-from flask_security import RoleMixin
 from ._base import db
 
 
-class Role(db.Model, RoleMixin):
+class Role(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True)
     description = db.Column(db.String(255))
--- flask_turbo_boost/api_project/application/models/user.py
+++ flask_turbo_boost/api_project/application/models/user.py
@@ -1,11 +1,10 @@
 # coding: utf-8
-from flask_security import UserMixin
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from ._base import db
 
 
-class User(db.Model, UserMixin):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), unique=True)
     email = db.Column(db.String(255), unique=True)
@@ -16,13 +15,6 @@ class User(db.Model, UserMixin):
 
     is_admin = db.Column(db.Boolean, default=False)
     created_at = db.Column(db.DateTime, default=datetime.now)
-
-    # Social Auth Provider, Facebook, Google
-    auth_provider_id = db.Column(db.String(255))
-    auth_provider_user_id = db.Column(db.String(255))
-    auth_provider_profile_pic = db.Column(db.String(512))
-    auth_provider_access_token = db.Column(db.String(255))
-    auth_provider_secret = db.Column(db.String(255))
 
     # relationship
     roles = db.relationship("Role", secondary="users_roles", backref='users')
