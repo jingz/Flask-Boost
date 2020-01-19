feaf221b090e0000de34a77c253dfc5134a565d8
jingz <wsaryoo@gmail.com>
2020-01-19 17:09:26 +0700

    enhance base model

--- flask_turbo_boost/project/application/models/_base.py
+++ flask_turbo_boost/project/application/models/_base.py
@@ -1,3 +1,11 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy.model import Model
+from application.extensions.searchable_mixin import SearchableMixin
+from application.extensions.active_record_mixin import ActiveRecordMixin
 
-db = SQLAlchemy()
+
+class BaseModelMixin(Model, ActiveRecordMixin, SearchableMixin):
+    pass
+
+
+db = SQLAlchemy(model_class=BaseModelMixin)
